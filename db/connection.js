17b2745889d04297c34dd26ae3980a5b30436d78
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-22 01:26:41 -0700

    updated database name to company_DB

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,17 @@ const mysql = require("mysql");
 
 // Create the connection information for the sql database
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3001,
-  //   YOUR username
-  user: "root",
-  //   YOUR password
-  password: "",
-  database: "employee_DB",
-});
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Be sure to update with your own MySQL password!
+    password: "password",
+    database: "company_DB",
+  });
 
 module.exports = connection;

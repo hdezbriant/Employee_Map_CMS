860bf8d6567cb067e6649d139a4e30738e8fcbaf
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-22 01:25:37 -0700

    Created structure for employee_DB sql database

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE employee_DB;
+USE employee_DB;
+
+CREATE TABLE department(
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(50),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(50),
+    salary INTEGER,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
+)
+
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+    role_id INT,
+	department_id INT,
+    manager_id INT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
+);

c7d18caa07f6894090b1334a4c1a738ea2f55248
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-22 06:42:21 -0700

    removed port definition, not needed.

--- db/connection.js
+++ db/connection.js
@@ -3,13 +3,8 @@ const mysql = require("mysql");
 // Create the connection information for the sql database
 const connection = mysql.createConnection({
     host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
     // Your username
     user: "root",
-  
     // Be sure to update with your own MySQL password!
     password: "password",
     database: "company_DB",
